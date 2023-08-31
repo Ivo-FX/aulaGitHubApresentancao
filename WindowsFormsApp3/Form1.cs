7e557d2f9d7ec0f3b7b15a5829260253a0a5699b
Ivo-Xavier1 <ivo.xavier@viavarejo.com.br>
2023-08-31 19:00:07 -0300

    Add project files.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //  System.Windows.Forms.MessageBox.Show("mensagem do Botão");
+            //string texto = "Mensagem do botão";
+
+            minhaBlioteca.minhaClasse minhaClasse = new minhaBlioteca.minhaClasse();
+
+            MessageBox.Show(minhaClasse.texto);
+        }
+    }
+}
+
+namespace minhaBlioteca
+{
+    public class minhaClasse
+    {
+        public string texto = "mensagem do botão 2";
+    }
+}
+
